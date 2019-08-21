bfb777ad94ddaffc6944f11cdad169e99065e0cd
max-k <maxgoogelt@gmail.com>
2019-08-21 13:24:16 +0200

    class will fill fomrs later on

---
+++ classes/project/FillForm.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once('classes/DBAccess.php');
+
+class FillForm {
+
+	private $keywords;
+
+	function __construct($form_type) {
+		$this->receive_keywords();
+	}
+
+	private function receive_keywords() {
+	
+	}
+
+}
+
+?>
