8456a2b3c66945a7feb3228d3d679b00f156604b
max-k <maxgoogelt@gmail.com>
2019-08-21 11:34:55 +0200

    added files for editin the tables

---
+++ files/neuerAuftrag.php
@@ -0,0 +1,22 @@
+<?php 
+require_once('classes/project/FormGenerator.php');
+
+$getTableData = array();
+if (ISSET($_POST['getTable'])) {
+	$type = $_POST['getTable'];
+	$column_names = DBAccess::selectQuery("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'${type}'");
+
+	for ($i = 0; $i < sizeof($column_names); $i++) {
+		$showColumnName = $column_names[$i]["COLUMN_NAME"];
+		$data = $_POST[$showColumnName];
+		array_push($getTableData, $data);
+	}
+
+	FormGenerator::insertData($type, $getTableData);
+	$table = FormGenerator::createTable("auftrag", true, true, "neuerAuftrag");
+	echo $table;
+} else {
+	$table = FormGenerator::createTable("auftrag", true, true, "neuerAuftrag");
+	echo "<div id='tableContainer'>" . $table . "</div>";
+}
+?>
