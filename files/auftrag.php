b915b0429d34d0010dd3c9d9e652409580ce3da0
max-k <maxgoogelt@gmail.com>
2019-08-23 09:48:12 +0200

    added select field in html to add different types of Posten

--- files/auftrag.php
+++ files/auftrag.php
@@ -4,7 +4,7 @@
 	$auftragsId = -1;
 	if (isset($_GET['id'])) {
 		$auftragsId = $_GET['id'];
-		$Auftrag = new Auftrag();
+		$Auftrag = new Auftrag($auftragsId);
 	}
 ?>
 
@@ -12,5 +12,19 @@
 	<input type="number" min="1" oninput="document.getElementById('auftragsLink').href = '<?=$auftragAnzeigen?>?id=' + this.value;">
 	<a href="#" id="auftragsLink">Auftrag anzeigen</a>
 <?php else: ?>
-	<div></div>
+	<div>
+		<span>Auftragsnummer: <?=$Auftrag->getAuftragsnummer()?></span>
+		<span>Beschreibung: <?=$Auftrag->getAuftragsbeschreibung()?></span>
+		<span>Schritte: <?=$Auftrag->getBearbeitungsschritte()?></span>
+		<span>Posten: <?=$Auftrag->getAuftragsposten()?></span>
+	</div>
+	<div>
+		<select id="selectPosten">
+			<option value="zeit">Zeit</option>
+			<option value="leistung">Leistung</option>
+			<option value="produkt">Produkt</option>
+		</select>
+		<div id="selectProdukt"></div>
+		<button>Posten hinzufügen</button>
+	</div>
 <?php endif; ?>
---
+++ files/res/js/auftrag.js
@@ -0,0 +1,4 @@
+function getSelection() {
+    var e = document.getElementById("selectPosten");
+    var strUser = e.options[e.selectedIndex].value;
+}
