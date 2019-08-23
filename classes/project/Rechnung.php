40d8dd20cd41de224ec86cb0e06c10732c9d3b14
max-k <maxgoogelt@gmail.com>
2019-08-23 18:27:53 +0200

    implemented class

--- classes/project/Rechnung.php
+++ classes/project/Rechnung.php
@@ -2,67 +2,30 @@
 
 error_reporting(E_ALL);
 
-/**
- * unbenanntesModell - class.Rechnung.php
- *
- * $Id$
- *
- * This file is part of unbenanntesModell.
- *
- * Automatically generated on 20.08.2019, 12:32:17 with ArgoUML PHP module 
- * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
- *
- * @author firstname and lastname of author, <author@example.org>
- */
-
 if (0 > version_compare(PHP_VERSION, '5')) {
     die('This file was generated for PHP 5');
 }
 
-/**
- * include Formular
- *
- * @author firstname and lastname of author, <author@example.org>
- */
-require_once('class.Formular.php');
+require_once('Auftrag.php');
 
-/* user defined includes */
-// section -64--88--78-22--dbaf7cb:16c8686fa2d:-8000:00000000000009C7-includes begin
-// section -64--88--78-22--dbaf7cb:16c8686fa2d:-8000:00000000000009C7-includes end
+class Rechnung extends Auftrag {
 
-/* user defined constants */
-// section -64--88--78-22--dbaf7cb:16c8686fa2d:-8000:00000000000009C7-constants begin
-// section -64--88--78-22--dbaf7cb:16c8686fa2d:-8000:00000000000009C7-constants end
+    private $rechnungsnummer = 0;
+	private $summeMwSt = 0;
+	private $summe = 0;
 
-/**
- * Short description of class Rechnung
- *
- * @access public
- * @author firstname and lastname of author, <author@example.org>
- */
-class Rechnung
-    extends Formular
-{
-    // --- ASSOCIATIONS ---
+	function __construct($auftragsnummer) {
+		parent::__construct($auftragsnummer);
+	}
 
-
-    // --- ATTRIBUTES ---
-
-    // --- OPERATIONS ---
-
-    /**
-     * Short description of method PDFgenerieren
-     *
-     * @access public
-     * @author firstname and lastname of author, <author@example.org>
-     * @return mixed
-     */
-    public function PDFgenerieren()
-    {
-        // section -64--88--78-22--dbaf7cb:16c8686fa2d:-8000:00000000000009D6 begin
-        // section -64--88--78-22--dbaf7cb:16c8686fa2d:-8000:00000000000009D6 end
+    public function PDFgenerieren() {
+        
     }
 
-} /* end of class Rechnung */
+	public function getRechnungsnummer() {
+		return $this->rechnungsnummer;
+	}
+
+}
 
 ?>
