e47e2e8493695e678f96a519a991ce60e83121de
max-k <maxgoogelt@gmail.com>
2019-08-23 18:28:38 +0200

    added links to produkt and rechnung

--- files/main.php
+++ files/main.php
@@ -3,7 +3,7 @@
 	
 	$neuerKunde   =  Link::getPageLink("neuer-kunde");
 	$neuerAuftrag =  Link::getPageLink("neuer-auftrag");
-	$neueRechnung =  Link::getPageLink("neue-rechnung");
+	$rechnung =  Link::getPageLink("rechnung");
 	$neuesAngebot =  Link::getPageLink("neues-angebot");
 	$neuesProdukt =  Link::getPageLink("neues-produkt");
 
@@ -13,7 +13,8 @@
 <div>
 	<a href="<?=$neuerKunde?>">Neuen Kunden erstelen</a>
 	<a href="<?=$neuerAuftrag?>">Neuen Auftrag erstellen</a>
-	<a href="<?=$neueRechnung?>">Neue Rechnung erstellen</a>
+	<a href="<?=$rechnung?>">Neue Rechnung erstellen</a>
+	<input type="number" min="1" oninput="document.getElementById('rechnungsLink').href = '<?=$rechnung?>?id=' + this.value;"><a href="#" id="rechnungsLink">Rechnung anzeigen</a>
 	<a href="<?=$neuesAngebot?>">Neues Angebot erstellen</a>
 	<a href="<?=$neuesProdukt?>">Neues Produkt erstellen</a>
 	<input type="number" min="1" oninput="document.getElementById('auftragsLink').href = '<?=$auftragAnzeigen?>?id=' + this.value;"><a href="#" id="auftragsLink">Auftrag anzeigen</a>
