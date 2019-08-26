c41281a1784fb9c337b0e709d7a3bcbd5a5cfaed
max-k <maxgoogelt@gmail.com>
2019-08-26 18:58:24 +0200

    getSelection() gets an empty table to add Posten, showSelection shows the Posten where the condition istStandart is 1

--- files/res/js/auftrag.js
+++ files/res/js/auftrag.js
@@ -1,4 +1,27 @@
+let buttons = document.getElementsByTagName("button");
+buttons[0].addEventListener("click", getSelection, false);
+buttons[1].addEventListener("click", function () { showSelection(event.target); }, false);
+
 function getSelection() {
     var e = document.getElementById("selectPosten");
     var strUser = e.options[e.selectedIndex].value;
+
+    if (strUser != "produkt") {
+        insertPosten = new AjaxCall(`getReason=createTable&type=${strUser}&showData=false`, "POST", window.location.href);
+        insertPosten.makeAjaxCall(function (responseTable) {
+            document.getElementById("addPosten").innerHTML = responseTable;
+
+            addableTables();
+        });
+    }
+}
+
+function showSelection(element) {
+    document.getElementById('newPosten').style.display = 'inline';
+    element.style.display = 'none';
+
+    getGeneralPosten = new AjaxCall("getReason=createTable&type=custom", "POST", window.location.href);
+    getGeneralPosten.makeAjaxCall(function (responseTable) {
+        document.getElementById("generalPosten").innerHTML = responseTable;
+    });
 }
