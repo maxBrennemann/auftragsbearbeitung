1600e8ffbdf3db16321af2c918ef152251c29d4a
max-k <maxgoogelt@gmail.com>
2019-08-26 19:00:27 +0200

    link to diagrams.php added

--- files/main.php
+++ files/main.php
@@ -6,16 +6,19 @@
 	$rechnung =  Link::getPageLink("rechnung");
 	$neuesAngebot =  Link::getPageLink("neues-angebot");
 	$neuesProdukt =  Link::getPageLink("neues-produkt");
-
+	$diagramme =  Link::getPageLink("diagramme");
 	$auftragAnzeigen = Link::getPageLink("auftrag");
 ?>
 
 <div>
-	<a href="<?=$neuerKunde?>">Neuen Kunden erstelen</a>
-	<a href="<?=$neuerAuftrag?>">Neuen Auftrag erstellen</a>
-	<a href="<?=$rechnung?>">Neue Rechnung erstellen</a>
-	<input type="number" min="1" oninput="document.getElementById('rechnungsLink').href = '<?=$rechnung?>?id=' + this.value;"><a href="#" id="rechnungsLink">Rechnung anzeigen</a>
-	<a href="<?=$neuesAngebot?>">Neues Angebot erstellen</a>
-	<a href="<?=$neuesProdukt?>">Neues Produkt erstellen</a>
-	<input type="number" min="1" oninput="document.getElementById('auftragsLink').href = '<?=$auftragAnzeigen?>?id=' + this.value;"><a href="#" id="auftragsLink">Auftrag anzeigen</a>
+	<ul>
+		<li><a href="<?=$neuerKunde?>">Neuen Kunden erstelen</a></li>
+		<li><a href="<?=$neuerAuftrag?>">Neuen Auftrag erstellen</a></li>
+		<li><a href="<?=$rechnung?>">Neue Rechnung erstellen</a></li>
+		<li><input type="number" min="1" oninput="document.getElementById('rechnungsLink').href = '<?=$rechnung?>?id=' + this.value;"><a href="#" id="rechnungsLink">Rechnung anzeigen</a></li>
+		<li><a href="<?=$neuesAngebot?>">Neues Angebot erstellen</a></li>
+		<li><a href="<?=$neuesProdukt?>">Neues Produkt erstellen</a></li>
+		<li><input type="number" min="1" oninput="document.getElementById('auftragsLink').href = '<?=$auftragAnzeigen?>?id=' + this.value;"><a href="#" id="auftragsLink">Auftrag anzeigen</a></li>
+		<li><a href="<?=$auftragAnzeigen?>">Diagramme und Auswertungen</a></li>
+	</ul>
 </div>
