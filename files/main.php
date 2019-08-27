526e5cfcd67d047836cdb86c5aee10a153419dc7
max-k <maxgoogelt@gmail.com>
2019-08-27 12:54:48 +0200

    changed line ending

--- files/main.php
+++ files/main.php
@@ -1,13 +1,13 @@
 <?php
 	require_once('classes/Link.php');
 	
-	$neuerKunde   =  Link::getPageLink("neuer-kunde");
-	$neuerAuftrag =  Link::getPageLink("neuer-auftrag");
-	$rechnung =  Link::getPageLink("rechnung");
-	$neuesAngebot =  Link::getPageLink("neues-angebot");
-	$neuesProdukt =  Link::getPageLink("neues-produkt");
-	$diagramme =  Link::getPageLink("diagramme");
-	$auftragAnzeigen = Link::getPageLink("auftrag");
+	$neuerKunde   =		Link::getPageLink("neuer-kunde");
+	$neuerAuftrag =		Link::getPageLink("neuer-auftrag");
+	$rechnung =			Link::getPageLink("rechnung");
+	$neuesAngebot =		Link::getPageLink("neues-angebot");
+	$neuesProdukt =		Link::getPageLink("neues-produkt");
+	$diagramme =		Link::getPageLink("diagramme");
+	$auftragAnzeigen =	Link::getPageLink("auftrag");
 ?>
 
 <div>
@@ -19,6 +19,6 @@
 		<li><a href="<?=$neuesAngebot?>">Neues Angebot erstellen</a></li>
 		<li><a href="<?=$neuesProdukt?>">Neues Produkt erstellen</a></li>
 		<li><input type="number" min="1" oninput="document.getElementById('auftragsLink').href = '<?=$auftragAnzeigen?>?id=' + this.value;"><a href="#" id="auftragsLink">Auftrag anzeigen</a></li>
-		<li><a href="<?=$auftragAnzeigen?>">Diagramme und Auswertungen</a></li>
+		<li><a href="<?=$diagramme?>">Diagramme und Auswertungen</a></li>
 	</ul>
 </div>
