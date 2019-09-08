c4fda39194611406ded4662e107cd8a68bd667e1
max-k <maxgoogelt@gmail.com>
2019-09-08 17:30:26 +0200

    Revert "added search and implemented some features like adding new auftraege and so on"
    This reverts commit ac3f18c375aef7ee11007978e9678a153d712f51.

--- classes/project/Aufgabenliste.php
+++ classes/project/Aufgabenliste.php
@@ -2,33 +2,12 @@
 
 error_reporting(E_ALL);
 
-if (0 > version_compare(PHP_VERSION, '5')) {
-    die('This file was generated for PHP 5');
+class Aufgabenliste {
+    
+	public static function aktuelleSchritteAlsTabelleAusgeben() {
+		echo "";
+	}
+
 }
-
-/**
- * Short description of class Aufgabenliste
- *
- * @access public
- * @author firstname and lastname of author, <author@example.org>
- */
-class Aufgabenliste
-{
-    // --- ASSOCIATIONS ---
-
-
-    // --- ATTRIBUTES ---
-
-    /**
-     * Short description of attribute Schritte
-     *
-     * @access public
-     * @var Schritte
-     */
-    public $Schritte = null;
-
-    // --- OPERATIONS ---
-
-} /* end of class Aufgabenliste */
 
 ?>
--- files/res/js/auftrag.js
+++ files/res/js/auftrag.js
@@ -1,15 +1,23 @@
 let buttons = document.getElementsByTagName("button");
 buttons[0].addEventListener("click", getSelection, false);
-buttons[1].addEventListener("click", function () { showSelection(event.target); }, false);
-buttons[2].addEventListener("click", addBearbeitungsschritte, false);
+buttons[1].addEventListener("click", performSearch, false);
+buttons[2].addEventListener("click", function () { showSelection(event.target); }, false);
+buttons[3].addEventListener("click", addBearbeitungsschritte, false);
 
 function getSelection() {
     var e = document.getElementById("selectPosten");
     var strUser = e.options[e.selectedIndex].value;
 
     if (strUser != "produkt") {
-        insertPosten = new AjaxCall(`getReason=createTable&type=${strUser}&showData=false`, "POST", window.location.href);
+        var insertPosten = new AjaxCall(`getReason=createTable&type=${strUser}&showData=false`, "POST", window.location.href);
         insertPosten.makeAjaxCall(function (responseTable) {
+            document.getElementById("addPosten").innerHTML = responseTable;
+
+            addableTables();
+        });
+    } else if (strUser == "produkt") {
+        var showProducts = new AjaxCall(`getReason=createTable&type=produkt`, "POST", window.location.href);
+        showProducts.makeAjaxCall(function (responseTable) {
             document.getElementById("addPosten").innerHTML = responseTable;
 
             addableTables();
@@ -21,16 +29,26 @@ function showSelection(element) {
     document.getElementById('newPosten').style.display = 'inline';
     element.style.display = 'none';
 
-    getGeneralPosten = new AjaxCall("getReason=createTable&type=custom", "POST", window.location.href);
+    var getGeneralPosten = new AjaxCall("getReason=createTable&type=custom", "POST", window.location.href);
     getGeneralPosten.makeAjaxCall(function (responseTable) {
         document.getElementById("generalPosten").innerHTML = responseTable;
     });
 }
 
 function addBearbeitungsschritte() {
-    bearbeitungsschritte = new AjaxCall("getReason=createTable&type=schritte", "POST", window.location.href);
+    var bearbeitungsschritte = new AjaxCall("getReason=createTable&type=schritte", "POST", window.location.href);
     bearbeitungsschritte.makeAjaxCall(function (responseTable) {
         document.getElementById("bearbeitungsschritte").innerHTML = responseTable;
+        addableTables();
+    });
+}
+
+function performSearch(e) {
+    var query = e.target.previousSibling.value;
+    console.log(query);
+    var search = new AjaxCall(`getReason=search&query=${query}&stype=produkt`, "POST", window.location.href);
+    search.makeAjaxCall(function (responseTable) {
+        document.getElementById("searchResults").innerHTML = responseTable;
         addableTables();
     });
 }
