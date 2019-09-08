eeef833b5030599730ad5c78c9ae25a9ad984e6f
max-k <maxgoogelt@gmail.com>
2019-09-08 17:18:55 +0200

    reformatted customer data, which is now also editable

--- files/kunde.php
+++ files/kunde.php
@@ -11,18 +11,21 @@
 <?php if ($kundenid == -1) : ?>
 	<p>Kunde kann nicht angezeigt werden.</p>
 <?php else: ?>
-	<div>
-		<span>Kundennummer: <?=$kunde->getKundennummer()?></span><br>
-		<span>Vorname: <?=$kunde->getVorname()?></span><br>
-		<span>Nachname: <?=$kunde->getNachname()?></span><br>
-		<span>Firmenname: <?=$kunde->getFirmenname()?></span><br>
-		<span>Straße: <?=$kunde->getStrasse()?></span><br>
-		<span>Hausnummer: <?=$kunde->getHausnummer()?></span><br>
-		<span>Postleitzahl: <?=$kunde->getPostleitzahl()?></span><br>
-		<span>Ort: <?=$kunde->getOrt()?></span><br>
-		<span>Email: <?=$kunde->getEmail()?></span><br>
-		<span>Telefon Festnetz: <?=$kunde->getTelefonFestnetz()?></span><br>
-		<span>Telefon Mobil: <?=$kunde->getTelefonMobil()?></span><br>
+	<div id="showKundendaten">
+		<ul>
+			<li>Kundennummer: <span><?=$kunde->getKundennummer()?></span></li>
+			<li>Vorname: <span class="editable" contenteditable data-col="Vorname"><?=$kunde->getVorname()?></span></li>
+			<li>Nachname: <span class="editable" contenteditable data-col="Nachname"><?=$kunde->getNachname()?></span></li>
+			<li>Firmenname: <span class="editable" contenteditable data-col="Firmenname"><?=$kunde->getFirmenname()?></span></li>
+			<li>Straße: <span class="editable" contenteditable data-col="Strasse"><?=$kunde->getStrasse()?></span></li>
+			<li>Hausnummer: <span class="editable" contenteditable data-col="Hausnummer"><?=$kunde->getHausnummer()?></span></li>
+			<li>Postleitzahl: <span class="editable" contenteditable data-col="Postleitzahl"><?=$kunde->getPostleitzahl()?></span></li>
+			<li>Ort: <span class="editable" contenteditable data-col="Ort"><?=$kunde->getOrt()?></span></li>
+			<li>Email: <span class="editable" contenteditable data-col="Email"><?=$kunde->getEmail()?></span></li>
+			<li>Telefon Festnetz: <span class="editable" contenteditable data-col="TelefonFestnetz"><?=$kunde->getTelefonFestnetz()?></span></li>
+			<li>Telefon Mobil: <span class="editable" contenteditable data-col="TelefonMobil"><?=$kunde->getTelefonMobil()?></span></li>
+		</ul>
+		<button id="sendKundendaten" disabled onclick="kundendatenAbsenden()">Absenden</button>
 	</div>
 	<div id="showFarben"><?=$kunde->getFarben()?></div>
 	<div id="showAuftraege"><?=$kunde->getAuftraege()?></div>
