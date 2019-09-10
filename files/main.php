d01644f3e2238644bab6bb8a7be3fb04f9f02386
max-k <maxgoogelt@gmail.com>
2019-09-10 11:17:03 +0200

    fixed orthogonal error

--- files/main.php
+++ files/main.php
@@ -16,7 +16,7 @@
 
 <div>
 	<ul>
-		<li><a href="<?=$neuerKunde?>">Neuen Kunden erstelen</a></li>
+		<li><a href="<?=$neuerKunde?>">Neuen Kunden erstellen</a></li>
 		<li><input type="number" min="1" oninput="document.getElementById('kundenLink').href = '<?=$kunde?>?id=' + this.value;"><a href="#" id="kundenLink">Kunde anzeigen</a></li>
 		<li><a href="<?=$neuerAuftrag?>">Neuen Auftrag erstellen</a></li>
 		<li><a href="<?=$rechnung?>">Neue Rechnung erstellen</a></li>
