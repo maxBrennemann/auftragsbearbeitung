4c7497f195ccc41aaf329af967b174fadd924f4d
max-k <maxgoogelt@gmail.com>
2019-09-20 17:57:08 +0200

    Leistung will be read from database and will be shown accroding to this as a dropdown menu

---
+++ files/leistungen.php
@@ -0,0 +1,36 @@
+﻿<?php
+require_once('classes/DBAccess.php');
+
+$data = DBAccess::selectQuery("SELECT * FROM leistung");
+?>
+
+<style>
+	.leistungsblock {
+		border: 1px solid black;
+		border-radius: 6px;
+		margin: 10px;
+		padding: 10px;
+	}
+</style>
+
+<a href="#" class="abutton">Folie konfigurieren</a>
+<a href="#" class="abutton">T-Shirt konfigurieren</a>
+
+<div>
+	<?php foreach ($data as $leistung): ?>
+		<div class="leistungsblock">
+			<h4><?=$leistung['Bezeichnung']?></h4>
+			<p>Beschreibung: <?=$leistung['Beschreibung']?></p>
+			<p>Quelle: <?=$leistung['Quelle']?></p>
+			<p>Aufschlag: <?=$leistung['Aufschlag']?>%</p>
+			<button onclick="remove('<?=$leistung['Nummer']?>')">🗑</button>
+		</div>
+	<?php endforeach; ?>
+	<div class="leistungsblock">
+		<span>Bezeichnung: <input type="text"  maxlength="32"></input></span><br>
+		<span>Beschreibung: <input type="text"></input></span><br>
+		<span>Quelle: <input type="text" maxlength="64"></input></span><br>
+		<span>Aufschlag: <input type="number"></input></span><br>
+		<button onclick="add()">Hinzufügen</button>
+	<div>
+</div>
