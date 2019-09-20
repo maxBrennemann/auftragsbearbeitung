3ed6ccc404db492966e5537c4b27cbed882d4c68
max-k <maxgoogelt@gmail.com>
2019-09-20 17:56:12 +0200

    file is used to add new Leistungen

---
+++ files/res/js/leistungen.js
@@ -0,0 +1,12 @@
+function remove(id) {
+    console.log(id)
+
+    var remove = new AjaxCall("", "POST", window.location.href);
+    remove.makeAjaxCall(function (responseLink) {
+        
+    });
+}
+
+function add() {
+    
+}
