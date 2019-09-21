d9d2c69d7e0c2a75cf50256341b14b1893f38d23
max-k <maxgoogelt@gmail.com>
2019-09-21 16:34:20 +0200

    setters and getters and buttons were added to edit tables on the ui

--- classes/project/InteractiveFormGenerator.php
+++ classes/project/InteractiveFormGenerator.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 require_once('classes/DBAccess.php');
 error_reporting(E_ALL);
@@ -8,21 +8,79 @@ if (0 > version_compare(PHP_VERSION, '5')) {
 }
 
 /**
- * Klasse generiert Tabellen f�r Formulare, erbt von FormGenerator
+ * Klasse generiert Tabellen für Formulare, erbt von FormGenerator
  *
  * @access public
  * @author Max Brennemann, maxgoogelt@gmail.com
  */
 class InteractiveFormGenerator extends FormGenerator {
-	
-	private $type;
-	private $isOrderedBy;
-	private $whereCondition;
-	private $tableData;
-	private $dataTypes = null;
 
-	function __construct($type, $isOrderedBy, $whereCondition) {
-		super($type, $isOrderedBy, $whereCondition);
+	private $isRowDeleatable = false;
+	private $isRowEditable = false;
+
+	private $identifier = "";
+
+	function __construct($type) {
+		parent::__construct($type, "", "");
+	}
+
+	public function setIsOrderedBy($isOrderedBy) {
+		if (is_string($val)) {
+			$this->isOrderedBy = $isOrderedBy;
+		} else {
+			throw new Exception("wrong data type, String required");
+		}
+	}
+
+	public function setWhereCondition($whereCondition) {
+		if (is_string($val)) {
+			$this->whereCondition = $whereCondition;
+		} else {
+			throw new Exception("wrong data type, String required");
+		}
+	}
+
+	public function setRowEditable($val) {
+		if (is_bool($val)) {
+			$this->isRowEditable = $val;
+		} else {
+			throw new Exception("wrong data type, boolean required");
+		}
+	}
+
+	public function setRowDeletable($val) {
+		if (is_bool($val)) {
+			$this->isRowDeletable = $val;
+		} else {
+			throw new Exception("wrong data type, boolean required");
+		}
+	}
+
+	public function editRow($row, $column, $data) {
+		DBAccess::updateQuery("UPDATE $type SET $column = $data WHERE $this->identifier = $row");
+	}
+
+	private function addDeleteButton($row) {
+		$button = "<button onclick=\"deleteRow('$this->type', $row)\">🗑</button>";
+		return $button;
+	}
+
+	private function addEditButton() {
+	
+	}
+
+	public function create($data, $columnNames) {
+		$editcolumn = array("COLUMN_NAME" => "Bearbeitung");
+		array_push($columnNames, $editcolumn);
+
+		for ($i = 0; $i < sizeof($data); $i++) {
+			$btn =  $this->addDeleteButton($i);
+			$data[$i]["Bearbeitung"] = $btn;
+		}
+
+		$_SESSION['data'] = serialize($data);
+
+		return $this->createTableByData($data, $columnNames);
 	}
 
 }
