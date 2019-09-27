5b04f0ed92a381ea167e1454fcbdceae02d6014b
max-k <maxgoogelt@gmail.com>
2019-09-27 13:57:47 +0200

    added function getOffeneRechnungen and getOffeneRechnungssumme

--- classes/project/Rechnung.php
+++ classes/project/Rechnung.php
@@ -40,6 +40,34 @@ class Rechnung extends Auftrag {
 		return $number;
 	}
 
+	public static function getOffeneRechnungen() {
+		$column_names = array(0 => array("COLUMN_NAME" => "Auftragsnummer"), 1 => array("COLUMN_NAME" => "Kundennummer"), 2 => array("COLUMN_NAME" => "Firmenname"),
+				3 => array("COLUMN_NAME" => "Auftragsbezeichnung"), 4 => array("COLUMN_NAME" => "Auftragsbeschreibung"), 5 => array("COLUMN_NAME" => "Datum"), 
+				6 => array("COLUMN_NAME" => "Termin"), 7 => array("COLUMN_NAME" => "AngenommenDurch"));
+		$data = DBAccess::selectQuery("SELECT auftrag.*, kunde.Firmenname FROM auftrag LEFT JOIN kunde ON auftrag.Kundennummer = kunde.Kundennummer WHERE Rechnungsnummer != 0 AND Bezahlt = 0");
+
+		for ($i = 0; $i < sizeof($data); $i++) {
+			$id = $data[$i]["AngenommenDurch"];
+			$angenommenDurch = DBAccess::selectQuery("SELECT Vorname, Nachname FROM mitarbeiter WHERE id = $id");
+			$data[$i]["AngenommenDurch"] = $angenommenDurch[0]["Vorname"] . " " . $angenommenDurch[0]["Nachname"];
+		}
+
+		$form = new FormGenerator("auftrag", "Datum", "Rechnungsnummer = 0");
+		$table = $form->createTableByDataRowLink($data, $column_names, "auftrag", null);
+		return $table;
+	}
+
+	public static function getOffeneRechnungssumme() {
+		$numbers = DBAccess::selectQuery("SELECT Auftragsnummer FROM auftrag WHERE Rechnungsnummer != 0");
+		$summe = 0;
+		foreach ($numbers as $auftrag) {
+			$auftrag = new Auftrag($auftrag['Auftragsnummer']);
+			$summe += $auftrag->preisBerechnen();
+		}
+
+		return $summe;
+	}
+
 }
 
 ?>
