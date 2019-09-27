78768bb993189eabb6cb1d701b962a7cf99d2cf3
max-k <maxgoogelt@gmail.com>
2019-09-27 13:55:19 +0200

    small changes

--- files/main.php
+++ files/main.php
@@ -2,6 +2,7 @@
 	require_once('classes/Link.php');
 	require_once('classes/project/Aufgabenliste.php');
 	require_once('classes/project/Auftrag.php');
+	require_once('classes/project/Rechnung.php');
 	
 	$neuerKunde   =		Link::getPageLink("neuer-kunde");
 	$neuerAuftrag =		Link::getPageLink("neuer-auftrag");
@@ -15,6 +16,8 @@
 
 	$showAktuelleSchritte = Aufgabenliste::aktuelleSchritteAlsTabelleAusgeben();
 	$showOffeneAuftraege = Auftrag::getOffeneAuftraege();
+	$showOffeneRechnungen = Rechnung::getOffeneRechnungen();
+	$offeneSumme = Rechnung::getOffeneRechnungssumme();
 
 	if (isset($_GET['showDetails'])) {
 		$showDetails = $_GET['showDetails'];
@@ -26,22 +29,21 @@
 		}
 	}
 ?>
-
 <div>
 	<ul>
-		<li><a href="<?=$neuerKunde?>">Neuen Kunden erstellen</a></li>
+		<li><a href="<?=$neuerKunde?>">+ Neuen Kunden erstellen</a></li>
 		<li>
 			<input id="kundeninput" type="number" min="1" oninput="document.getElementById('kundenLink').href = '<?=$kunde?>?id=' + this.value;">
 			<a href="#" id="kundenLink">Kunde anzeigen</a>
 		</li>
-		<li><a href="<?=$neuerAuftrag?>">Neuen Auftrag erstellen</a></li>
+		<li><a href="<?=$neuerAuftrag?>">+ Neuen Auftrag erstellen</a></li>
 		<li><a href="<?=$rechnung?>">Neue Rechnung erstellen</a></li>
 		<li>
 			<input id="rechnungsinput" type="number" min="1" oninput="document.getElementById('rechnungsLink').href = '<?=$rechnung?>?id=' + this.value;">
 			<a href="#" id="rechnungsLink">Rechnung anzeigen</a>
 		</li>
-		<li><a href="<?=$neuesAngebot?>">Neues Angebot erstellen</a></li>
-		<li><a href="<?=$neuesProdukt?>">Neues Produkt erstellen</a></li>
+		<li><a href="<?=$neuesAngebot?>">+ Neues Angebot erstellen</a></li>
+		<li><a href="<?=$neuesProdukt?>">+ Neues Produkt erstellen</a></li>
 		<li>
 			<input id="auftragsinput" type="number" min="1" oninput="document.getElementById('auftragsLink').href = '<?=$auftragAnzeigen?>?id=' + this.value;">
 			<a href="#" id="auftragsLink">Auftrag anzeigen</a>
@@ -51,7 +53,8 @@
 	</ul>
 
 	<div>
-		<?=$showAktuelleSchritte?>
-		<p>Offene Aufträge:</p><?=$showOffeneAuftraege?>
+		<p><u>Offene Bearbeitungsschritte:</u></p><?=$showAktuelleSchritte?>
+		<p><u>Offene Aufträge:</u></p><?=$showOffeneAuftraege?>
+		<p><u>Offene Rechnungen:</u> <?=$offeneSumme?>€</p><?=$showOffeneRechnungen?>
 	</div>
 </div>
