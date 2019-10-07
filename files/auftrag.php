ebc50341926cb244cc7e9a2324cf10cef1087a69
max-k <maxgoogelt@gmail.com>
2019-10-07 18:42:08 +0200

    added inputs to add colors, changed html markup, because its using grid layout now

--- files/auftrag.php
+++ files/auftrag.php
@@ -1,48 +1,74 @@
-<?php 
+<?php
 	require_once('classes/project/Auftrag.php');
+	require_once('classes/project/Rechnung.php');
 	require_once('classes/project/FormGenerator.php');
+	require_once('classes/project/Kunde.php');
 	require_once('classes/DBAccess.php');
 
 	$auftragsId = -1;
 	if (isset($_GET['id'])) {
 		$auftragsId = $_GET['id'];
 		try {
-			$Auftrag = new Auftrag($auftragsId);
-			$fahrzeuge = DBAccess::selectQuery("SELECT Kennzeichen, Fahrzeug FROM fahrzeuge WHERE Kundennummer = {$Auftrag->getKundennummer()}");
-			$column_names = array(0 => array("COLUMN_NAME" => "Kennzeichen"), 1 => array("COLUMN_NAME" => "Fahrzeug"));
-			$table = new FormGenerator("Fahrzeuge", "", "");
-			$table = $table->createTableByData($fahrzeuge, $column_names);
-		} catch (Exception $e){
+			$auftrag = new Auftrag($auftragsId);
+			$fahrzeugTable = $auftrag->getFahrzeuge();
+			$farbTable = $auftrag->getFarben();
+			
+			$kunde = new Kunde($auftrag->getKundennummer());
+		} catch (Exception $e) {
 			echo $e->getMessage();
 			$auftragsId = -1;
 		}
 	}
 
-	$leistungen = DBAccess::selectQuery("SELECT Bezeichnung, Nummer FROM leistung");
-?>
+	if(isset($_GET['create'])) {
+		$nextId = Rechnung::getNextNumber();
+		$auftragsid = $_GET['create'];
+		echo "Rechnung $nextId wird erstellt";
+		DBAccess::updateQuery("UPDATE auftrag SET Rechnungsnummer = $nextId WHERE Auftragsnummer = $auftragsid");
+	}
 
-<?php if ($auftragsId == -1) : ?>
+	$leistungen = DBAccess::selectQuery("SELECT Bezeichnung, Nummer FROM leistung");
+
+if ($auftragsId == -1) : ?>
 	<input type="number" min="1" oninput="document.getElementById('auftragsLink').href = '<?=$auftragAnzeigen?>?id=' + this.value;">
 	<a href="#" id="auftragsLink">Auftrag anzeigen</a>
+<?php elseif ($auftrag->istRechnungGestellt()) : ?>
+	<p>Auftrag <?=$auftrag->getAuftragsnummer()?> wurde abgeschlossen. Rechnungsnummer: <span id="rechnungsnummer"><?=$auftrag->getRechnungsnummer()?></span></p>
+	<button onclick="print('rechnungsnummer', 'Rechnung');">Rechnungsblatt anzeigen</button>
 <?php else: ?>
-	<a href="<?=Link::getPageLink("kunde")?>?id=<?=$Auftrag->getKundennummer()?>">Kunde <span id="kundennummer"><?=$Auftrag->getKundennummer()?></span> zeigen</a>
-	<div>
-		<span><u>Auftragsnummer:</u> <?=$Auftrag->getAuftragsnummer()?></span><br>
-		<span><u>Beschreibung:</u><br><?=$Auftrag->getAuftragsbeschreibung()?></span><br>
+	<aside class="border">
+		<?=$kunde->getVorname()?> <?=$kunde->getNachname()?><br><?=$kunde->getFirmenname()?><br>Adresse: <br><?=$kunde->getStrasse()?> <?=$kunde->getHausnummer()?><br>
+		<?=$kunde->getPostleitzahl()?> <?=$kunde->getOrt()?><br><a href="mailto:<?=$kunde->getEmail()?>"><?=$kunde->getEmail()?></a><br>
+		<a href="<?=Link::getPageLink("kunde")?>?id=<?=$auftrag->getKundennummer()?>">Kunde <span id="kundennummer"><?=$auftrag->getKundennummer()?></span> zeigen</a>
+	</aside>
+	<div class="border auftragsinfo">
+		<span><u>Auftragsnummer:</u> <span id="auftragsnummer"><?=$auftrag->getAuftragsnummer()?></span></span><br>
+		<span><button onclick="print('auftragsnummer', 'Auftrag');">Auftragsblatt anzeigen</button></span>
+		<span><button onclick="rechnungErstellen();">Rechnung generieren</button></span><br>
+		<span><u>Beschreibung:</u><br><?=$auftrag->getAuftragsbeschreibung()?></span><br>
+	</div>
+	<div class="border schritte">
 		<span><u>Schritte:</u><br>
 			<form name="showSteps">
 				<input onchange="radio('hide')" type="radio" name="showDone" value="hide" checked> Zu erledigende Schritte anzeigen<br>
 				<input onchange="radio('show')" type="radio" name="showDone" value="show"> Alle Schritte anzeigen<br>
 			</form>
-			<span id="stepTable"><?=$Auftrag->getOpenBearbeitungsschritteAsTable()?></span>
-		</span><br>
-		<span><u>Posten:</u> <?=$Auftrag->getAuftragspostenAsTable()?></span>
-		<span><u>Gesamtpreis:</u> <?=$Auftrag->preisBerechnen()?>€</span><br>
-		<span><u>Fahrzeuge:</u> <?=$table?></span>
+			<span id="stepTable"><?=$auftrag->getOpenBearbeitungsschritteAsTable()?></span>
+		</span>
 	</div>
-	<br>
-	<hr>
-	<div id="newPosten" style="display: none;">
+	<div class="border posten">
+		<span><u>Posten:</u> <?=$auftrag->getAuftragspostenAsTable()?></span>
+	</div>
+	<div class="border preis">
+		<span><u>Gesamtpreis:</u> <?=$auftrag->preisBerechnen()?>€</span>
+	</div>
+	<div class="border fahrzeuge">
+		<span><u>Fahrzeuge:</u> <?=$fahrzeugTable?></span>
+	</div>
+	<div class="border farben">
+		<span><u>Farben:</u> <?=$farbTable?></span>
+	</div>
+	<div class="border postenadd" id="newPosten">
 		<select id="selectPosten">
 			<option value="zeit">Zeit</option>
 			<option value="leistung">Leistung</option>
@@ -75,14 +101,24 @@
 				</div>
 			</div>
 		</div>
+		<div id="generalPosten"></div>
+	</div>
+	<div class="border produkt">
 		<div id="selectProdukt">
-			<hr>
 			<span>Produkt suchen: <input type="text"><button onclick="performSearch()">&#x1F50E;</button></span>
 			<div id="searchResults"></div>
 		</div>
-		<div id="generalPosten"></div>
 	</div>
-	<button onclick="showSelection(event.target)">Neuen Posten hinzufügen</button>
-	<button onclick="addBearbeitungsschritte()">Neuen Bearbeitungsschritt hinzufügen</button>
-	<div id="bearbeitungsschritte"></div>
+	<div class="border step">
+		<button onclick="addBearbeitungsschritte()">Neuen Bearbeitungsschritt hinzufügen</button>
+		<div id="bearbeitungsschritte"></div>
+		<button onclick="addColor()">Neuen Farbe hinzufügen</button>
+		<div id="farbe" style="display: none">
+			<br>
+			<span>Farbname: <input class="colorInput" type="text" max="32"></span><br>
+			<span>Farbe (Hex): <input class="colorInput jscolor" type="text" max="32"></span><br>
+			<span>Bezeichnung: <input class="colorInput" type="text" max="32"></span><br>
+			<span>Hersteller: <input class="colorInput" tyep="text" max="32"></span><br>
+		</div>
+	</div>
 <?php endif; ?>
