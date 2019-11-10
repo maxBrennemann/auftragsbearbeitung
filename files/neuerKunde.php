8fb363661159e66e6218e1d1044bccfcef910bef
max-k <maxgoogelt@gmail.com>
2019-11-10 22:49:32 +0100

    added direct implementation

--- files/neuerKunde.php
+++ files/neuerKunde.php
@@ -41,3 +41,77 @@ if (isset($_POST['getTable'])) {
 	</div>
 	<button onclick="addDataToDB()">Hinzufügen</button>
 <? endif; ?>
+<div>
+	<form>
+		<p>
+			<label>Firmenname
+				<input class="dataInput" type="text" name="firmenname">
+			</label>
+		</p>
+		<p>
+			<label>Anrede
+				<select id="selectAnrede">
+					<option value="0">Herr</option>
+					<option value="1">Frau</option>
+					<option value="2">Firma</option>
+				</select>
+			</label>
+		</p>
+		<p>
+			<label>Vorname
+				<input class="dataInput" type="text" name="vorname">
+			</label>
+		</p>
+		<p>
+			<label>Nachname
+				<input class="dataInput" type="text" name="nachname">
+			</label>
+		</p>
+		<p>
+			<label>Straße
+				<input class="dataInput" type="text" name="strasse">
+			</label>
+		</p>
+		<p>
+			<label>Hausnummer
+				<input class="dataInput" type="text" name="hausnummer">
+			</label>
+		</p>
+		<p>
+			<label>Postleitzahl
+				<input class="dataInput" type="number" name="plz">
+			</label>
+		</p>
+		<p>
+			<label>Ort
+				<input class="dataInput" type="text" name="ort">
+			</label>
+		</p>
+		<p>
+			<label>Email
+				<input class="dataInput" type="email" name="emailadress">
+			</label>
+		</p>
+		<p>
+			<label>Telefon Festnetz
+				<input class="dataInput" type="tel" name="telfestnetz" pattern="[0-9]{5}\/[0-9]{+}">
+			</label>
+		</p>
+		<p>
+			<label>Telefon Mobil
+				<input class="dataInput" type="text" name="telmobil">
+			</label>
+		</p>
+
+		<input type="submit">
+	</form>
+</div>
+<style>
+	form p {
+		display: block;
+	}
+
+	.dataInput {
+		float: right;
+	}
+</style>
