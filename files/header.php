22038eaac444c8af2126d9a7e61c67916b51aa99
maxBrennemann <maxgoogelt@gmail.com>
2019-11-14 15:37:21 +0100

    added meta name viewport

--- files/header.php
+++ files/header.php
@@ -22,6 +22,7 @@
 <html>
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, height=device-height">
 	<title>b-schriftung - <?=$pageName?></title>
 	<meta name="Description" content="Auftragsübersicht">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
