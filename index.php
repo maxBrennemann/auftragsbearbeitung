8e7413788e9077d968165486aa1aad16242394bd
maxBrennemann <maxgoogelt@gmail.com>
2019-11-14 16:01:24 +0100

    added new global variable SUB_URL this variable is resposable for a sub url

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	*/
 	$url = $_SERVER['REQUEST_URI'];
 	$url = explode('?', $url, 2);
-	$page = str_replace(REWRITE_BASE . "content/", "", $url[0]);
+	$page = str_replace(REWRITE_BASE . SUB_URL, "", $url[0]);
 	$parts = explode('/', $page);
 	$page = $parts[count($parts) - 1];
 	if($parts[0] == 'artikel') {
