e3b196c0fff2b2f002a7debea096f52f728faf7a
max-k <maxgoogelt@gmail.com>
2019-12-01 14:36:38 +0100

    used to add propositions

---
+++ files/toDo.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('classes/project/FormGenerator.php');
+
+    $verbesserungen = DBAccess::selectQuery("SELECT verbesserungen AS Verbesserungen, erledigt FROM verbesserungen");
+    $column_names = array(0 => array("COLUMN_NAME" => "Verbesserungen"), 1 => array("COLUMN_NAME" => "erledigt"));
+    $table = new FormGenerator("", "", "");
+    echo $table->createTableByData($verbesserungen, $column_names);
+?>
+<input type="text" id="verbesserung">
+<input type="button" id="submit" value="Abschicken">
+<script>
+    document.getElementById("submit").addEventListener("click", function(event) {
+        var data = document.getElementById("verbesserung").value;
+        var getHTML = new AjaxCall(`getReason=insertVerbesserung&verbesserung=${data}`, "POST", window.location.href);
+        getHTML.makeAjaxCall(function (response) {
+            location.realod();
+        });
+    });
+</script>
