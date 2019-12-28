0fe530e1734a479c1834870e866a5496834e5391
max-k <maxgoogelt@gmail.com>
2019-12-28 17:50:59 +0100

    changes in css

--- files/neuerAuftrag.php
+++ files/neuerAuftrag.php
@@ -20,39 +20,85 @@
 
 
 <?php if ($kdnr != -1) : ?>
-	<span>Kundennummer: <?=$kdnr?></span><br>
-	<span>Name: <?=$kundendaten['Vorname']?> <?=$kundendaten['Nachname']?></span><br>
-	<span>Firma: <?=$kundendaten['Firmenname']?></span><br>
-	<span>Auftragsbezeichnung: <input id="bezeichnung" maxlength="255"></span><br>
-	<span>Auftragsbeschreibung: <textarea id="beschreibung" maxlength="65535"></textarea></span><br>
-	<span>Auftragstyp: <input id="typ"></span><br>
-	<span>Termin: <input id="termin" type="date"></span><br>
-	<span>Angenommen durch: 
-		<select id="selectMitarbeiter">
-			<?php foreach ($mitarbeiter as $m): ?>
-				<option value="<?=$m['id']?>"><?=$m['Vorname']?> <?=$m['Nachname']?></option>
-			<?php endforeach; ?>
-		</select>
-	</span><br>
-	<span>Angenommen per: 
-		<select id="selectAngenommen">
-			<?php foreach ($annahme as $m): ?>
-				<option value="<?=$m['id']?>"><?=$m['Bezeichnung']?></option>
-			<?php endforeach; ?>
-		</select>
-	</span><br>
-	<?php if (sizeOf($ansprechpartner) > 0) : ?>
-	<span>Ansprechpartner: 
-		<select id="selectAnsprechpartner">
-			<?php foreach ($ansprechpartner as $m): ?>
-				<option value="<?=$m['Nummer']?>"><?=$m['Vorname']?> <?=$m['Nachname']?></option>
-			<?php endforeach; ?>
-		</select>
-	</span><br>
-	<?php endif; ?>
-	<button onclick="auftragHinzufuegen()">Absenden</button>
+	<div class="addOrder">
+		<div>
+			<span>Kundennummer: <?=$kdnr?></span><br>
+			<span>Name: <?=$kundendaten['Vorname']?> <?=$kundendaten['Nachname']?></span><br>
+			<span>Firma: <?=$kundendaten['Firmenname']?></span><br>
+			<span>Kurzbeschreibung: <input id="bezeichnung" maxlength="255"></span><br>
+			<span>Auftragsbeschreibung: <br><textarea id="beschreibung" maxlength="65535"></textarea></span><br>
+			<span>Auftragstyp: <input id="typ" placeholder="Textil | Fahrzeugbeschriftung | ..."></span><br>
+			<span>Termin: <input id="termin" type="date"></span><br>
+			<span>Angenommen durch: 
+				<select id="selectMitarbeiter">
+					<?php foreach ($mitarbeiter as $m): ?>
+						<option value="<?=$m['id']?>"><?=$m['Vorname']?> <?=$m['Nachname']?></option>
+					<?php endforeach; ?>
+				</select>
+			</span><br>
+			<span>Angenommen per: 
+				<select id="selectAngenommen">
+					<?php foreach ($annahme as $m): ?>
+						<option value="<?=$m['id']?>"><?=$m['Bezeichnung']?></option>
+					<?php endforeach; ?>
+				</select>
+			</span><br>
+			<?php if (sizeOf($ansprechpartner) > 0) : ?>
+			<span>Ansprechpartner: 
+				<select id="selectAnsprechpartner">
+					<?php foreach ($ansprechpartner as $m): ?>
+						<option value="<?=$m['Nummer']?>"><?=$m['Vorname']?> <?=$m['Nachname']?></option>
+					<?php endforeach; ?>
+				</select>
+			</span><br>
+			<?php endif; ?>
+		</div>
+		<button id="absenden" onclick="auftragHinzufuegen()">Absenden</button>
+	</div>
 <?php else: ?>
 	<span>Kundennummer oder Suchen: <input id="kundensuche" onkeyup="performSearchEnter(event, this.value);"><button onclick="performSearchButton(event)">&#x1F50E;</button></span>
 	<input type="number" min="1" id="auftragsnummer"><button onclick="print('auftragsnummer', 'Auftrag');">Auftragsblatt generieren</button>
 	<span id="searchResults"></span>
 <?php endif; ?>
+<style>
+	.addOrder {
+		border-radius: 6px;
+		margin: 10px;
+		background-color: #eff0f1;
+	}
+
+	.addOrder > div {
+		padding: 15px;
+	}
+
+	input {
+		border: none;
+		border-radius: 6px;
+		padding: 6px;
+		margin: 4px;
+	}
+
+	textarea {
+		border: none;
+		border-radius: 6px;
+		padding: 6px;
+	}
+
+	::placeholder {
+		font-size: 0.8em;
+		font-style: italic;
+	}
+
+	#absenden {
+		width: 100%;
+		margin: 0;
+		padding: 12px;
+		border-radius: 0 0 6px 6px;
+		border: none;
+		background: #B2B2BE;
+	}
+
+	#selectMitarbeiter, #selectAngenommen, #selectAnsprechpartner {
+		float: right;
+	}
+</style>
