a469f570660e0cd75c6188a1f64442a1aceeda90
maxBrennemann <maxgoogelt@gmail.com>
2020-01-11 11:39:55 +0100

    tried to fix upload

--- sqlChanges.sql
+++ sqlChanges.sql
@@ -35,7 +35,14 @@ ALTER TABLE `kunde_extended` ADD FOREIGN KEY (`kundennummer`) REFERENCES `kunde`
 /* Änderungen 28.12.2019 */
 CREATE TRIGGER createRowForNewCustomer AFTER INSERT ON kunde FOR EACH ROW INSERT INTO kunde_extended SET kunde_extended.kundennummer = NEW.Kundennummer;
 
-/* Änderungen 06.01.2019 */
+/* Änderungen 06.01.2020 */
 ALTER TABLE farben CHANGE Nummer Nummer INT(10) AUTO_INCREMENT;
 ALTER TABLE farben ADD Hersteller varchar(64) NOT NULL;
 INSERT INTO articles (id, articleUrl, pageName, src) VALUES (16, 'offeneRechnungen.php', 'Offene Rechnungen', "offene-rechnungen");
+
+/* Änderungen 08.01.2020 */
+ALTER TABLE posten ADD ohneBerechnung INT(10) NOT NULL;
+
+/* Änderungen 10.01.2020*/
+ALTER TABLE dateien DROP FOREIGN KEY dateien_ibfk_1;
+ALTER TABLE `dateien` DROP `auftragsnummer`;
