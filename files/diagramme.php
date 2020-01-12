587000cbe59280059382ee767d1656f4cc6c29df
max-k <maxgoogelt@gmail.com>
2020-01-12 14:28:13 +0100

    added new statisitcs, shows now the accepted orders by labors

--- files/diagramme.php
+++ files/diagramme.php
@@ -1,20 +1,33 @@
 <?php
 	require_once('classes/project/FormGenerator.php');
+	require_once('classes/Link.php');
 
-	$d = new DateTime('first day of this month');
-	echo $d->format("Y-m-d");
-
+	$diagram = isset($_GET['type']) ? $_GET['type'] : "default";
 	$sqlQueries = [
 		0 => 'SELECT DISTINCT COUNT(auftrag.Kundennummer) AS Anzahl, kunde.Vorname, kunde.Nachname, kunde.Firmenname FROM auftrag LEFT JOIN kunde ON kunde.Kundennummer = auftrag.Kundennummer GROUP BY auftrag.Kundennummer',
-		1 => ''
+		1 => "SELECT CONCAT(mitarbeiter.Vorname, ' ', mitarbeiter.Nachname) AS `Mitarbeiter`, COUNT(*) AS 'Angenommene Aufträge' FROM auftrag LEFT JOIN mitarbeiter ON auftrag.AngenommenDurch = mitarbeiter.id GROUP BY `Mitarbeiter`"
 	];
-
 	$table = new FormGenerator("", "", "");
-	$data = DBAccess::selectQuery($sqlQueries[0]);
-	$column_names = array(0 => array("COLUMN_NAME" => "Anzahl"), 1 => array("COLUMN_NAME" => "Vorname"),
-		2 => array("COLUMN_NAME" => "Nachname"), 3 => array("COLUMN_NAME" => "Firmenname"));
-	$table = $table->createTableByData($data, $column_names);
 
-	echo "<h4>Anzahl der Bestellungen pro Kunde:</h4>";
-	echo $table;
+	switch ($diagram) {
+		case "mitarbeiter":
+			$data = DBAccess::selectQuery($sqlQueries[1]);
+			$column_names = array(0 => array("COLUMN_NAME" => "Mitarbeiter"), 1 => array("COLUMN_NAME" => "Angenommene Aufträge"));
+			$table = $table->createTableByData($data, $column_names);
+
+			echo "<h4>Anzahl der Angenommenen Aufträge pro Mitarbeiter:</h4>";
+			echo $table;
+		break;
+		default:
+			$data = DBAccess::selectQuery($sqlQueries[0]);
+			$column_names = array(0 => array("COLUMN_NAME" => "Anzahl"), 1 => array("COLUMN_NAME" => "Vorname"),
+				2 => array("COLUMN_NAME" => "Nachname"), 3 => array("COLUMN_NAME" => "Firmenname"));
+			$table = $table->createTableByData($data, $column_names);
+
+			echo "<h4>Anzahl der Bestellungen pro Kunde:</h4>";
+			echo $table;
+	}
 ?>
+<br><br>
+<a href="<?=Link::getPageLink('diagramme')?>">Anzahl der Bestellungen pro Kunde</a><br>
+<a href="<?=Link::getPageLink('diagramme')?>?type=mitarbeiter">Anzahl der Angenommenen Aufträge pro Mitarbeiter</a><br>
