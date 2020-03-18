0bdb389f994c645e8d6972ad727850c37c583a63
Max Brennemann <maxgoogelt@gmail.com>
2020-03-18 12:22:47 +0100

    added php backend implementation for listmaker currently the functions are empty

---
+++ classes/project/Liste.php
@@ -0,0 +1,26 @@
+<?php 
+
+class Liste {
+
+    private $listenpunkte = array();
+    private $name = "";
+
+    function __construct($name) {
+		$this->name = $name;
+    }
+    
+    public function createList() {
+
+    }
+
+    public function addListenPunkt() {
+
+    }
+
+    public function removeListenPunkt() {
+
+    }
+
+}
+
+?>
---
+++ classes/project/Listenpunkt.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Listenpunkt {
+
+    private $listenauswahl = array();
+
+    function __construct() {
+		
+    }
+    
+    public function addListenAuswahl() {
+
+    }
+
+    public function removeListenAuswahl() {
+        
+    }
+
+}
+
+?>
