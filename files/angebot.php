e84ff119266c2adccef4370e72677a0923e5b4fb
Max Brennemann <maxgoogelt@gmail.com>
2020-03-18 12:23:24 +0100

    added implemenation for showing offers

--- files/angebot.php
+++ files/angebot.php
@@ -2,15 +2,32 @@
 	require_once('classes/Link.php');
 	require_once('classes/project/FormGenerator.php');
 	require_once('classes/project/Angebot.php');
-	
-	//$a = new Angebot();
-	//$a->pdfGenerieren();
+
+	$showAngebot = 0;
+
+	if (isset($_GET['id'])) {
+		$id = (int) $_GET['id'];
+		if ($id <= 0) {
+			echo "Angebotsnummer existiert nicht.";
+		} else {
+			$showAngebot = $id;
+		}
+	}
 
 	if (isset($_GET['open'])) :
+		$offeneAngebote = DBAccess::selectQuery("SELECT angebot.id, IF(kunde.Firmenname = '', CONCAT(kunde.Vorname, ' ', kunde.Nachname), kunde.Firmenname) as Name FROM angebot, kunde WHERE status = 0 AND angebot.kdnr = kunde.Kundennummer");
 ?>
 
 <h2>Offene Angebote</h2>
+<div class="defCont">
+	<?php foreach ($offeneAngebote as $angebot): ?>
+		<span><a href="<?=Link::getPageLink("angebot")?>?id=<?=$angebot['id']?>">Angebot <?=$angebot['id']?></a> für Kunde  <?=$angebot['Name']?></span><br>
+	<?php endforeach; ?>
+</div>
+<!-- offeneAngeboteEnde -->
+<?php elseif ($showAngebot != 0): ?>
 
+<!-- showAngebotEnde -->
 <?php else : ?>
 <div class="defCont">
 	<a href="<?=Link::getPageLink("angebot")?>?open">Offene Angebote durchsehen</a>
