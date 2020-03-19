93330d23848172fd91a72a82d68ddac58e5c910b
Max Brennemann <maxgoogelt@gmail.com>
2020-03-19 15:00:30 +0100

    added implementation for lists, stil a bug inside saves data now to db

--- classes/project/Listenauswahl.php
+++ classes/project/Listenauswahl.php
@@ -1,7 +1,22 @@
-<?php 
+<?php
+
+require_once('classes/DBAccess.php');
 
 class Listenauswahl {
     
+    private $bezeichnung;
+    private $isChecked;
+    private $text;
+    private $ordnung;
+
+    function __construct($bezeichnung, $ordnung) {
+        $this->bezeichnung = $bezeichnung;
+        $this->ordnung = $ordnung;
+    }
+
+    public function saveList($listenpunktid) {
+        DBAccess::insertQuery("INSERT INTO listenauswahl (listenpunktid, `bezeichnung`, ordnung) VALUES ($listenpunktid, '{$this->bezeichnung}', {$this->ordnung})");
+    }
 }
 
 ?>
--- classes/project/Listenpunkt.php
+++ classes/project/Listenpunkt.php
@@ -1,19 +1,35 @@
-<?php 
+<?php
+
+require_once('classes/DBAccess.php');
+require_once('classes/project/Listenauswahl.php');
 
 class Listenpunkt {
 
     private $listenauswahl = array();
+    private $text;
+    private $ordnung;
+    private $art;
 
-    function __construct() {
-		
+    function __construct($text, $art, $ordnung) {
+        $this->text = $text;
+        $this->art = $art;
+        $this->ordnung = $ordnung;
     }
     
-    public function addListenAuswahl() {
-
+    public function addListenAuswahl($aw) {
+        array_push($this->listenauswahl, $aw);
     }
 
     public function removeListenAuswahl() {
         
+    }
+
+    public function saveList($listenid) {
+        $id = DBAccess::insertQuery("INSERT INTO listenpunkt (`listenid`, `text`, `art`, `ordnung`) VALUES ($listenid, '{$this->text}', {$this->art}, {$this->ordnung})");
+    
+        foreach($this->listenauswahl as $la) {
+          $la->saveList($id);
+        }
     }
 
 }
