a47e4c5f35490f7f4e7192375509ee2c700171c2
Max Brennemann <maxgoogelt@gmail.com>
2020-03-21 16:29:21 +0100

    added attribute to product matcher, data still has to be stored

--- classes/project/AttributeGroup.php
+++ classes/project/AttributeGroup.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('classes/Files.php');
+
 class AttributeGroup {
 
     private $attributeGroup = null;
@@ -16,7 +18,41 @@ class AttributeGroup {
         } else {
             throw new Exception("Keine gültige Zahl übergeben.");
         }
-	}
+    }
+    
+    public static function getProductToAttributeMatcher() {
+        $fileContent = Files::get_file_contents("attributes.html");
+
+        $attributeGroups = DBAccess::selectQuery("SELECT id, attribute_group FROM attribute_group");
+        $anzahl = sizeof($attributeGroups);
+        $attr = "";
+
+        foreach ($attributeGroups as $agroup) {
+            $attr .= "<option value=\"{$agroup['id']}\">{$agroup['attribute_group']}</option>";
+        }
+
+        $replacements = array(
+            "ANZAHLATT" => $anzahl,
+            "ATTR" => $attr
+        );
+
+        $fileContent = str_replace("ANZAHLATT", $replacements["ANZAHLATT"], $fileContent);
+        $fileContent = str_replace("ATTR", $replacements["ATTR"], $fileContent);
+
+        echo $fileContent;
+    }
+
+    public static function getAttributes($attributeGroupId) {
+        $attributes = DBAccess::selectQuery("SELECT id, value FROM attribute WHERE attribute_group_id = $attributeGroupId");
+        $size = sizeof($attributes);
+        $html = "";
+
+        foreach ($attributes as $a) {
+            $html .= "<span class=\"selectSim\" onclick=\"addAttributeToProduct($attributeGroupId, {$a['id']}, '{$a['value']}');\">{$a['value']} ⊕</span><br>";
+        }
+
+        echo $html . "</select>";
+    }
 }
 
 ?>
