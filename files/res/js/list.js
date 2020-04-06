a2fae49b756b63158c70ab8d2b07e5b82c7c463d
Max Brennemann <maxgoogelt@gmail.com>
2020-04-07 00:05:11 +0200

    added lsit.js, startet implementing some methods

---
+++ files/res/js/list.js
@@ -0,0 +1,15 @@
+function addList() {
+    var el = document.getElementById("listauswahl");
+    el.style.display = "inline";
+
+    centerAbsoluteElement(el);
+    addActionButtonForDiv(el, 'hide');
+}
+
+function chooseList(listid) {
+
+}
+
+function saveListData() {
+
+}
