12c189f0150c007d47736d4adb8647b7c2bcdb36
Max Brennemann <maxgoogelt@gmail.com>
2020-04-11 12:25:31 +0200

    added implementation for empty function headers

--- files/res/js/list.js
+++ files/res/js/list.js
@@ -7,9 +7,14 @@ function addList() {
 }
 
 function chooseList(listid) {
-
+    var get = new AjaxCall(`getReason=getList&listId=${listid}`, "POST", window.location.href);
+    get.makeAjaxCall(function (response) {
+        var anchor = document.getElementsByClassName("liste")[0];
+        anchor.innerHTML = response;
+    });
 }
 
 function saveListData() {
-
+    var add = new AjaxCall(`getReason=saveListData&listId=${listid}&data=${data}`, "POST", window.location.href);
+    add.makeAjaxCall(function (response) {});
 }
