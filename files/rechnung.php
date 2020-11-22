595abdaad734439d7e4f0f334a345f7cf0bb44cc
Max Brennemann <maxgoogelt@gmail.com>
2020-11-22 13:34:18 +0100

    started implementing invoice pdf generation

--- files/rechnung.php
+++ files/rechnung.php
@@ -10,17 +10,29 @@
 		$rechnungsnummer = $_GET['id'];
 		$rechnung = new Rechnung($rechnungsnummer);
 	}
-	if(isset($_GET['create'])) {
-		$nextId = Rechnung::getNextNumber();
-		$auftragsid = $_GET['create'];
-		echo "Rechnung $nextId wird erstellt";
-		DBAccess::updateQuery("UPDATE auftrag SET Rechnungsnummer = $nextId WHERE Auftragsnummer = $auftragsid");
-	}
-?>
 
-<?php if ($rechnungsnummer == 0) : ?>
-	<p>Auftragsnummer: <input type="number" min="1" oninput="document.getElementById('rechnungsLink').href = '<?=$rechnung?>?create=' + this.value;"></p>
-	<a href="#" id="rechnungsLink">Rechnung generieren</a>
+	$setInvoiceGenerator = false;
+	if(isset($_GET['create'])) {
+		$auftragsid = $_GET['create'];
+		$setInvoiceGenerator = true;
+
+		$auftrag = new Auftrag($auftragsid);
+		$posten = $auftrag->getAuftragspostenAsTable();
+
+		$_SESSION['currentInvoice_orderId'] = $auftragsid;
+	}
+
+	$link = Link::getPageLink('pdf')  . "?type=rechnung";
+	$rechnungsPDF = "<iframe src=\"" . $link . "\" id=\"showOffer\"></iframe>";
+
+if ($setInvoiceGenerator): ?>
+	<div class="defCont">
+		<h3>Auftrag <?=$auftragsid?></h3>
+		<p>Posten zum Auftrag:</p>
+		<?=$posten?>
+	</div>
+	<button>Alle Posten übernehmen</button><button>Rechnung abschließen</button>
+	<span><?=$rechnungsPDF?></span>
 <?php else: ?>
 	<div>Rechnung:</div>
 	<span id="rechnungsnummer"><?=$rechnungsnummer;?></span>
