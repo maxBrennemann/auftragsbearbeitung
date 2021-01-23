be69d00e1fa3874c29a5e09d5c1e9586e4d74223
Max Brennemann <maxgoogelt@gmail.com>
2021-01-23 14:59:38 +0100

    added link creation for notification manager

--- classes/project/NotificationManager.php
+++ classes/project/NotificationManager.php
@@ -1,5 +1,14 @@
 <?php
 
+require_once('classes/Link.php');
+
+/*
+ * notification types:
+ *  0 -> undefined / unset
+ *  1 -> Bearbeitungsschritt
+ *  2 -> Posten
+ *  3 -> Datei
+ */
 class NotificationManager {
 
     /*
@@ -8,11 +17,11 @@ class NotificationManager {
     public static function getNotificationCount() {
         if (isset($_SESSION['userid'])) {
             $user = $_SESSION['userid'];
-            $result = DBAccess::selectQuery("SELECT COUNT(notification_id) FROM user_notifications WHERE user_id = $user AND ischecked = 'false'");
+            $result = DBAccess::selectQuery("SELECT COUNT(id) FROM user_notifications WHERE user_id = $user AND ischecked = 'false'");
             if ($result == null) {
                 return 0;
             } else {
-                return $result[0]["COUNT(notification_id)"];
+                return $result[0]["COUNT(id)"];
             }
         }
     }
@@ -27,14 +36,33 @@ class NotificationManager {
         $htmlContent = "";
 
         foreach ($data as $n) {
-            $htmlContent .= "<span>Art: <strong>" . $n['type'] . "</strong><br>" . $n['content'] . "</span>";
+            $type = self::getTypeName((int) $n['type']);
+            $link = self::getSpecificLink((int) $n['specific_id']);
+            $htmlContent .= "<span><strong>" . $type . ": </strong><a href=\"$link\">" . $n['content'] . "</a></span>";
         }
 
         return $htmlContent;
     }
 
-    public static function addNotification($user_id, $type, $content) {
-        DBAccess::insertQuery("INSERT INTO user_notifications (user_id, `type`, content) VALUES ($user_id, '$type', '$content')");
+    private static function getSpecificLink($type) {
+        switch ($type) {
+            case 1:
+                $orderId = DBAccess::selectQuery("SELECT Auftragsnummer FROM schritte WHERE SChrittnummer = $type")[0]['Auftragsnummer'];
+                return Link::getPageLink("auftrag") . "?id=$orderId";
+                break;
+            case 0:
+            default:
+                break;
+        }
+    }
+
+    private static function getTypeName($type) {
+        $types = ["", "Schritt"];
+        return $types[$type];
+    }
+
+    public static function addNotification($user_id, $type, $content, $specificId) {
+        DBAccess::insertQuery("INSERT INTO user_notifications (user_id, `type`, content, specific_id) VALUES ($user_id, $type, '$content', $specificId)");
     }
 
 }
