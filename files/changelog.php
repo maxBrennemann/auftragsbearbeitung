9931e3d9412ffd08842267c09ac780cfdeb443b1
Max Brennemann <maxgoogelt@gmail.com>
2021-03-23 12:23:13 +0100

    changelog

---
+++ files/changelog.php
@@ -0,0 +1,17 @@
+<div class="changelog">
+    <h3>1.1.7</h3>
+    <ul>
+        <li>Fahrzeuge werden nun über ein eigenes Menü hinzugefügt</li>
+        <li>Ansprechpartner können nun über den neuen Tabellengenerator hinzugefügt werden</li>
+        <li>Auf der Kundenseite werden die Aufträge nun als Karteikarten angezeigt</li>
+    </ul>
+</div>
+<style>
+    .changelog {
+
+    }
+
+    .changelog ul {
+        list-style-type: disc;
+    }
+</style>
