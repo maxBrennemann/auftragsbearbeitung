ccea54a071187dd563a7cffa1110ff26d2eb6bcb
Max Brennemann <maxgoogelt@gmail.com>
2021-03-26 09:44:18 +0100

    new changelog element

--- files/changelog.php
+++ files/changelog.php
@@ -4,6 +4,7 @@
         <li>Fahrzeuge werden nun über ein eigenes Menü hinzugefügt</li>
         <li>Ansprechpartner können nun über den neuen Tabellengenerator hinzugefügt werden</li>
         <li>Auf der Kundenseite werden die Aufträge nun als Karteikarten angezeigt</li>
+        <li>Bearbeitungsschritte auf der Frontpage können angeklickt werden - man wird zum Auftrag weitergeleitet</li>
     </ul>
 </div>
 <style>
