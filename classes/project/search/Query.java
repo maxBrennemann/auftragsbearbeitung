d85e2c930c9c913aecbf1ba9a278251a35ac400e
Max Brennemann <maxgoogelt@gmail.com>
2021-08-20 12:51:17 +0200

    deleted stuff

--- classes/project/search/Query.java
+++ classes/project/search/Query.java
@@ -21,6 +21,10 @@ public class Query {
 		
 		return s;
 	}
+
+	public String getQuery() {
+		return query;
+	}
 	
 	/**
 	 * Calculates the similarity (a number within 0 and 1) between two strings.
