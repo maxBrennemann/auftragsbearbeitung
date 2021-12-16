d1b62d7404d12445fce5ba6a72ae587eef36d743
Max Brennemann <maxgoogelt@gmail.com>
2021-12-16 12:50:26 +0100

    small changes and bugfixes: auftragsverlauf ends with specific node; if image not found, no image is shown; improvements for Verbesserungen

--- files/toDo.php
+++ files/toDo.php
@@ -3,6 +3,29 @@
     $link = Link::getPageLink("verbesserungen");
 ?>
 <p><a href="<?=$link?>?t=details">Details</a> oder <a href="<?=$link?>?t=unsolved">nicht erledigte</a> Verbesserungen.</p>
+<div class="defCont">
+    <p>Nachricht eingeben:</p>
+    <input id="verbesserung">
+    <br><br>
+    <input type="button" id="submit" value="Abschicken">
+    <script>
+        document.getElementById("submit").addEventListener("click", function(event) {
+            var data = document.getElementById("verbesserung").value;
+            var getHTML = new AjaxCall(`getReason=insertVerbesserung&verbesserung=${data}`, "POST", window.location.href);
+            getHTML.makeAjaxCall(function (response) {
+                location.reload();
+            });
+        });
+
+        let verbesserungsInput = document.getElementById("verbesserung");
+        verbesserungsInput.addEventListener("keyup", function (event) {
+            if (event.key === "Enter") {
+                document.getElementById("submit").click();
+            }
+        });
+    </script>
+</div>
+<br>
 <?php
     require_once('classes/project/FormGenerator.php');
 
@@ -18,14 +41,3 @@
     $table = new FormGenerator("", "", "");
     echo $table->createTableByData($verbesserungen, $column_names);
 ?>
-<textarea id="verbesserung"></textarea>
-<input type="button" id="submit" value="Abschicken">
-<script>
-    document.getElementById("submit").addEventListener("click", function(event) {
-        var data = document.getElementById("verbesserung").value;
-        var getHTML = new AjaxCall(`getReason=insertVerbesserung&verbesserung=${data}`, "POST", window.location.href);
-        getHTML.makeAjaxCall(function (response) {
-            location.reload();
-        });
-    });
-</script>
