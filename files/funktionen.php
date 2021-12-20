45dfcfe3ffb7e27a396e40f36d5e20a27e6b3916
Max Brennemann <maxgoogelt@gmail.com>
2021-12-20 09:54:49 +0100

    added overview over all functions

---
+++ files/funktionen.php
@@ -0,0 +1,47 @@
+<div class="defCont">
+    <h2>Funktionen der Auftragsbearbeitung</h2>
+    <ul>
+        <li>Kundenverwaltung</li>
+            <ul>
+                <li>Anlegen eines neuen Kunden</li>
+                <li>Kundendaten ändern</li>
+            </ul>
+        <li>Auftragsverwaltung</li>
+            <ul>
+                <li>Anlegen eines neuen Auftrags</li>
+                <li>Posten hinzufügen und bearbeiten</li>
+                <li>Bearbeitungsschritte und Notizen</li>
+                <li>Rechnungsstellung</li>
+            </ul>
+        <li>Angebote</li>
+            <ul>
+                <li>Anlegen eines neuen Angebots</li>
+                <li>Posten hinzufügen</li>
+                <li>Angebot in Auftrag übernehmen</li>
+            </ul>
+        <li>Rechnungen</li>
+        <li>Leistungen</li>
+        <li>Produkte</li>
+        <li>Analyse und Statistik</li>
+    </ul>
+    <h2>Funktionen der Besucherseite</h2>
+    <ul>
+        <li>Produkte</li>
+        <li>Warenkorb</li>
+        <li>Bestellvorgang</li>
+    </ul>
+</div>
+
+<style>
+    ul {
+        padding-left: inherit;
+    }
+
+    li {
+        list-style: disc;
+    }
+
+    .defCont {
+        width: 80vw;
+    }
+</style>
