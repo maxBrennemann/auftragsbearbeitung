335a0df8122408b2db4e2efa394854a413ac2b7c
Max Brennemann <maxgoogelt@gmail.com>
2022-06-01 09:43:27 +0200

    fixes lineheight issue, multiline descriptions do not break pdf, pages get added, when items are longer than the site limit, table header is now generated via an extra function and is displayed on every new page

--- classes/project/Rechnung.php
+++ classes/project/Rechnung.php
@@ -79,6 +79,7 @@ class Rechnung {
         $pdf->SetTitle('Rechnung für ' . $this->kunde->getFirmenname() . " " . $this->kunde->getName());
         $pdf->SetSubject('Angebot');
         $pdf->SetKeywords('pdf, angebot');
+		//$pdf->SetAutoPageBreak(true, 35);
 
 		$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 		$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
@@ -87,56 +88,61 @@ class Rechnung {
 
         $pdf->setCellPaddings(1, 1, 1, 1);
         $pdf->setCellMargins(0, 0, 0, 0);
+		$pdf->setMargins(25, 45, 20, true);
 
 		$pdf->SetFont("helvetica", "", 8);
 		$address = "<p>b-schriftung Brennemann Dietmar, Huberweg 31, 94522 Wallersdorf</p>";
-		$pdf->writeHTMLCell(0, 40, 20, 25, $address);
+		$pdf->writeHTMLCell(0, 40, 25, 25, $address);
 
 		$pdf->SetFont("helvetica", "", 12);
         $cAddress = "<p>{$this->kunde->getFirmenname()}<br>{$this->kunde->getName()}<br>{$this->kunde->getStrasse()} {$this->kunde->getHausnummer()}<br>{$this->kunde->getPostleitzahl()} {$this->kunde->getOrt()}</p>";
-        $pdf->writeHTMLCell(85, 40, 20, 25, $cAddress);
+        $pdf->writeHTMLCell(85, 40, 25, 25, $cAddress);
 
 		$pdf->setXY(120, 30);
 		$pdf->setFontStretching(200);
 		$pdf->SetFont("helvetica", "B", 17);
 		$pdf->Cell(60, 20, "RECHNUNG", 0, 0, 'L', 0, '', 2);
 		$pdf->setFontStretching();
-		$pdf->SetFont("helvetica", "", 12);
-		$pdf->setXY(120, 45);
-		$pdf->Cell(30, 10, "Rechnungs-Nr:");
-		$pdf->Cell(30, 10, $this->getInvoiceId(), 0, 0, 'R');
-		$pdf->setXY(120, 51);
-		$pdf->Cell(30, 10, "Datum:");
-		$pdf->Cell(30, 10, $this->getDate(), 0, 0, 'R');
-		$pdf->setXY(120, 57);
-		$pdf->Cell(30, 10, "Kunden-Nr.:");
-		$pdf->Cell(30, 10, $this->kunde->getKundennummer(), 0, 0, 'R');
-		$pdf->setXY(120, 63);
-		$pdf->Cell(30, 10, "Seite:");
-		$pdf->Cell(30, 10, $pdf->PageNo() . ' von ' . $pdf->getNumPages(), 0, 0, 'R');
 
-        $pdf->setXY(20, 90);
-		$pdf->SetFont("helvetica", "B", 12);
-        $pdf->Cell(20, 10, 'Menge', 'B');
-        $pdf->Cell(20, 10, 'MEH', 'B');
-        $pdf->Cell(80, 10, 'Bezeichnung', 'B');
-        $pdf->Cell(20, 10, 'E-Preis', 'B');
-		$pdf->Cell(20, 10, 'G-Preis', 'B');
-		$pdf->SetFont("helvetica", "", 12);
+		$this->addTableHeader($pdf);
 		
 		/* iterates over all posten and adds lines */
+		$lineheight = 10;
 		$this->loadPostenFromAuftrag();
-		$offset = 10;
+		$offset = 100;
+		$pdf->setXY(25, $offset);
 		if ($this->posten != null) {
 			foreach ($this->posten as $p) {
-				$pdf->setXY(20, 90 + $offset);
-				$pdf->Cell(20, 10, $p->getQuantity());
-				$pdf->Cell(20, 10, $p->getEinheit());
-				$pdf->Cell(80, 10, $p->getDescription());
-				$pdf->Cell(20, 10, number_format($p->bekommeEinzelPreis(), 2, ',', '') . ' €');
-				$pdf->Cell(20, 10, number_format($p->bekommePreis(), 2, ',', '') . ' €', 0, 0, 'R');
-				$offset += 10;
+				$pdf->Cell(20, $lineheight, $p->getQuantity());
+				$pdf->Cell(20, $lineheight, $p->getEinheit());
+
+				$height = $pdf->getStringHeight(80, $p->getDescription());
+				$addToOffset = $lineheight;
+				if ($height >= $lineheight) {
+					$pdf->MultiCell(80, $lineheight, $p->getDescription(), '', 'L', false, 0, null, null, true, 0, false, true, 0, 'B', false);
+					$addToOffset = ceil($height);
+				} else {
+					$pdf->Cell(80, $lineheight, $p->getDescription());
+				}
+
+				$pdf->Cell(20, $lineheight, number_format($p->bekommeEinzelPreis(), 2, ',', '') . ' €');
+				$pdf->Cell(20, $lineheight, number_format($p->bekommePreis(), 2, ',', '') . ' €', 0, 0, 'R');
+				
+				$offset += $addToOffset;
+				$pdf->ln($addToOffset);
+
+				/* 297: Din A4 Seitenhöhe, 25: Abstand von unten für die Fußzeile */
+				if ($pdf->GetY() + $addToOffset >= 297 - 25) {
+					$pdf->AddPage();
+					$this->addTableHeader($pdf, 25);
+					$pdf->ln(10);
+				}
 			}
+		}
+
+		/* 297: Din A4 Seitenhöhe, 25: Abstand von unten für die Fußzeile, 55: bezieht sich auf die Zwischensumme und Rechnungssumme, damit diese immer auf einer Seite stehen */
+		if ($pdf->GetY() + 55 >= 297 - 25) {
+			$pdf->AddPage();
 		}
 
 		/* Code für Zwischensumme und Rechnungssumme */
@@ -145,40 +151,36 @@ class Rechnung {
 		$mwst = number_format($summe * 0.19, 2, ',', '') . ' €';
 		$rechnungssumme = number_format($summe * 1.19, 2, ',', '') . ' €';
 
-        $pdf->setXY(20, 90 + $offset);
+		$pdf->ln();
 		$pdf->Cell(80, 10, "");
 		$pdf->SetFont("helvetica", "B", 12);
 		$pdf->Cell(60, 10, 'Zwischensumme:', 'T');
 		$pdf->SetFont("helvetica", "", 12);
 		$pdf->Cell(20, 10, $zwischensumme, 'T', 0, 'R');
 
-		$offset += 10;
-		$pdf->setXY(20, 90 + $offset);
+		$pdf->ln();
 		$pdf->Cell(80, 10, "");
 		$pdf->SetFont("helvetica", "B", 12);
 		$pdf->Cell(60, 10, '19% MwSt.:', 'B');
 		$pdf->SetFont("helvetica", "", 12);
 		$pdf->Cell(20, 10, $mwst, 'B', 0, 'R');
 
-		$offset += 10;
-		$pdf->setXY(20, 90 + $offset);
+		$pdf->ln();
 		$pdf->Cell(80, 10, "");
 		$pdf->SetFont("helvetica", "B", 12);
 		$pdf->Cell(60, 10, 'Rechnungssumme:', 'B');
 		$pdf->SetFont("helvetica", "", 12);
 		$pdf->Cell(20, 10, $rechnungssumme, 'B', 0, 'R');
 
-		$offset += 10;
+		$pdf->ln();
 		$pdf->setCellMargins(0, 1, 0, 0);
-		$pdf->setXY(20, 90 + $offset);
 		$pdf->Cell(80, 10, "");
 		$pdf->Cell(60, 10, '', 'T');
 		$pdf->Cell(20, 10, '', 'T');
 
 		/* Code für "Zahlungsziel 8 Tage" */
-		$offset += 10;
+		$pdf->ln();
 		$pdf->setCellMargins(0, 0, 0, 0);
-		$pdf->setXY(20, 90 + $offset);
 		$pdf->SetFont("helvetica", "", 10);
 		$pdf->Cell(160, 10, "Zahlungsziel 8 Tage");
 
@@ -192,6 +194,36 @@ class Rechnung {
 		} else {
 			$pdf->Output();
 		}
+	}
+
+	/**
+	 * fügt einen Tabellenkopf und die allgemeinen Rechnungsdaten ein;
+	 * @param TCPDF &$pdf Refernz auf PDF Variable
+	 * @param int $y Abstand zu oben, Standard ist 45, damit die erste Seite richtig generiert wird
+	 */
+	private function addTableHeader(&$pdf, $y = 45) {
+		$pdf->SetFont("helvetica", "", 12);
+		$pdf->setXY(120, $y);
+		$pdf->Cell(30, 10, "Rechnungs-Nr:");
+		$pdf->Cell(30, 10, $this->getInvoiceId(), 0, 0, 'R');
+		$pdf->setXY(120, $y + 6);
+		$pdf->Cell(30, 10, "Datum:");
+		$pdf->Cell(30, 10, $this->getDate(), 0, 0, 'R');
+		$pdf->setXY(120, $y + 12);
+		$pdf->Cell(30, 10, "Kunden-Nr.:");
+		$pdf->Cell(30, 10, $this->kunde->getKundennummer(), 0, 0, 'R');
+		$pdf->setXY(120, $y + 18);
+		$pdf->Cell(30, 10, "Seite:");
+		$pdf->Cell(30, 10, $pdf->getAliasRightShift() . $pdf->PageNo() . ' von ' . $pdf->getAliasNbPages(), 0, 0, 'R');
+
+        $pdf->setXY(25, $y + 45);
+		$pdf->SetFont("helvetica", "B", 12);
+        $pdf->Cell(20, 10, 'Menge', 'B');
+        $pdf->Cell(20, 10, 'MEH', 'B');
+        $pdf->Cell(80, 10, 'Bezeichnung', 'B');
+        $pdf->Cell(20, 10, 'E-Preis', 'B');
+		$pdf->Cell(20, 10, 'G-Preis', 'B');
+		$pdf->SetFont("helvetica", "", 12);
 	}
 
 	/*
