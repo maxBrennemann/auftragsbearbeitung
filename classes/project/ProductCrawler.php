474e1c26541a8022a6c4f0dd200ff16b9810915e
Max Brennemann <maxgoogelt@gmail.com>
2022-12-07 18:44:10 +0100

    added product crawler

---
+++ classes/project/ProductCrawler.php
@@ -0,0 +1,13 @@
+<?php 
+
+class ProductCrawler {
+
+    /**
+     * alle produkte durchgehen
+     * alle ids checken und wenn nicht existert, dann wird ein neuer eintrag erstellt
+     * daten werden eingetragen
+     */
+
+}
+
+?>
