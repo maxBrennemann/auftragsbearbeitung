5a3723964bd59ded18f7ec3b23cd6373af4bf49b
Max Brennemann <maxgoogelt@gmail.com>
2023-03-10 08:42:45 +0100

    settings example file

---
+++ settings-example.php
@@ -0,0 +1,21 @@
+<?php
+
+define('REWRITE_BASE', '');
+define('WEB_URL', '');
+define('SUB_URL', '');
+define('FRONT', '');
+
+define('ERRORREPORTING', true);
+define('CURRENTVERSION', '');
+
+/* database connection */
+define('HOST', '');
+define('USERNAME', '');
+define('PASSWORD', '');
+define('DATABASE', '');
+
+/* shop api keys */
+define('SHOPURL', '');
+define('SHOPKEY', '');
+
+?>
