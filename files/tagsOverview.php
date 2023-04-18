ab3479ca917fbc6755780854cb6338da09588d25
Max Brennemann <maxgoogelt@gmail.com>
2023-04-18 23:08:07 +0200

    added tag overview content

--- files/tagsOverview.php
+++ files/tagsOverview.php
@@ -6,4 +6,11 @@
 // neu crawlen btn einführen
 // tags häufigkeit und produkte dazu speichern
 
+$query = "SELECT `content` FROM module_sticker_tags";
+$tags = DBAccess::selectQuery($query);
+
 ?>
+<p>Anzahl Tags: <?=count($tags)?></p>
+<?php foreach ($tags as $tag): ?>
+    <dt class="inline-block py-1 px-2 border-none rounded-lg bg-cyan-500 m-2"><?=$tag["content"]?></dt>
+<?php endforeach; ?>
