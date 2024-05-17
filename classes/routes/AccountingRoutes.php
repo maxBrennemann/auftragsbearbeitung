4acdc809ba4251e09c9e2108ed4c46eca8569725
Max Brennemann <maxgoogelt@gmail.com>
2024-05-17 06:35:56 +0200

    added next accounting steps

---
+++ classes/routes/AccountingRoutes.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once("classes/routes/Routes.php");
+
+class AccountingRoutes extends Routes {
+
+    protected static $getRoutes = [
+    ];
+
+    protected static $postRoutes = [
+    ];
+
+    protected static $putRoutes = [
+    ];
+
+    protected static $deleteRoutes = [
+    ];
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+}
