1d629415a35a935dc0a4cdf37241366b5f0b9c31
Max Brennemann <maxgoogelt@gmail.com>
2024-11-11 23:08:49 +0100

    small step in continuing offers

--- files/angebot.php
+++ files/angebot.php
@@ -1,38 +1,11 @@
 <?php
 
 use Classes\Link;
-use MaxBrennemann\PhpUtilities\DBAccess;
+use Classes\Project\Offer;
 
-$showAngebot = 0;
-$showOffer = false;
+$openOffers = Offer::getOpenOffers();
 
-if (isset($_GET['id'])) {
-	$id = (int) $_GET['id'];
-	if ($id <= 0) {
-		echo "Angebotsnummer existiert nicht.";
-	} else {
-		$showAngebot = $id;
-	}
-}
-
-if (isset($_SESSION["offer_id"])) {
-	$showOffer = true;
-}
-
-if (isset($_GET['open'])) :
-	$offeneAngebote = DBAccess::selectQuery("SELECT angebot.id, IF(kunde.Firmenname = '', CONCAT(kunde.Vorname, ' ', kunde.Nachname), kunde.Firmenname) as Name FROM angebot, kunde WHERE status = 0 AND angebot.kdnr = kunde.Kundennummer");
 ?>
-<h2>Offene Angebote</h2>
-<div class="defCont">
-	<?php foreach ($offeneAngebote as $angebot): ?>
-		<span><a href="<?=Link::getPageLink("angebot")?>?id=<?=$angebot['id']?>">Angebot <?=$angebot['id']?></a> für Kunde  <?=$angebot['Name']?></span><br>
-	<?php endforeach; ?>
-</div>
-<!-- offeneAngeboteEnde -->
-<?php elseif ($showAngebot != 0): ?>
-
-<!-- showAngebotEnde -->
-<?php else : ?>
 <div class="defCont">
 	<a href="<?=Link::getPageLink("angebot")?>?open" class="link-primary">Offene Angebote durchsehen</a>
 	<p>Kundennummer:
@@ -42,4 +15,3 @@ if (isset($_GET['open'])) :
 	</p>
 </div>
 <div id="insTemp"></div>
-<?php endif; ?>
