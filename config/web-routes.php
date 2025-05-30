0b7b5376bcd70a072892b7a41a6b0b5c03bb7220
Max Brennemann <maxgoogelt@gmail.com>
2025-05-30 18:28:32 +0200

    added notification page

--- config/web-routes.php
+++ config/web-routes.php
@@ -1,132 +1,136 @@
 <?php
 
 return [
-    '' => [
-        'file' => 'main.php',
-        'name' => '',
+    "" => [
+        "file" => "main.php",
+        "name" => "",
     ],
-    'neuer-kunde' => [
-        'file' => 'neuerKunde.php',
-        'name' => 'Neuer Kunde',
+    "neuer-kunde" => [
+        "file" => "neuerKunde.php",
+        "name" => "Neuer Kunde",
     ],
-    'neuer-auftrag' => [
-        'file' => 'neuerAuftrag.php',
-        'name' => 'Neuer Auftrag',
+    "neuer-auftrag" => [
+        "file" => "neuerAuftrag.php",
+        "name" => "Neuer Auftrag",
     ],
-    'rechnung' => [
-        'file' => 'rechnung.php',
-        'name' => 'Rechnung',
+    "rechnung" => [
+        "file" => "rechnung.php",
+        "name" => "Rechnung",
     ],
-    'angebot' => [
-        'file' => 'angebot.php',
-        'name' => 'Angebot',
+    "angebot" => [
+        "file" => "angebot.php",
+        "name" => "Angebot",
     ],
-    'neues-produkt' => [
-        'file' => 'neuesProdukt.php',
-        'name' => 'Neues Produkt',
+    "neues-produkt" => [
+        "file" => "neuesProdukt.php",
+        "name" => "Neues Produkt",
     ],
-    '404' => [
-        'file' => '404.html',
-        'name' => '404 - Not found',
+    "404" => [
+        "file" => "404.html",
+        "name" => "404 - Not found",
     ],
-    'auftrag' => [
-        'file' => 'auftrag.php',
-        'name' => 'Auftrag',
+    "auftrag" => [
+        "file" => "auftrag.php",
+        "name" => "Auftrag",
     ],
-    'produkt' => [
-        'file' => 'produkt.php',
-        'name' => 'Produktinformationen',
+    "produkt" => [
+        "file" => "produkt.php",
+        "name" => "Produktinformationen",
     ],
-    'diagramme' => [
-        'file' => 'diagramme.php',
-        'name' => 'Diagramme',
+    "diagramme" => [
+        "file" => "diagramme.php",
+        "name" => "Diagramme",
     ],
-    'kunde' => [
-        'file' => 'kunde.php',
-        'name' => 'Kundeninfo',
+    "kunde" => [
+        "file" => "kunde.php",
+        "name" => "Kundeninfo",
     ],
-    'leistungen' => [
-        'file' => 'leistungen.php',
-        'name' => 'Leistungen',
+    "leistungen" => [
+        "file" => "leistungen.php",
+        "name" => "Leistungen",
     ],
-    'login' => [
-        'file' => 'login.php',
-        'name' => 'Login',
+    "login" => [
+        "file" => "login.php",
+        "name" => "Login",
     ],
-    'attributes' => [
-        'file' => 'attributes.php',
-        'name' => 'Attribute',
+    "attributes" => [
+        "file" => "attributes.php",
+        "name" => "Attribute",
     ],
-    'offene-rechnungen' => [
-        'file' => 'offeneRechnungen.php',
-        'name' => 'Offene Rechnungen',
+    "offene-rechnungen" => [
+        "file" => "offeneRechnungen.php",
+        "name" => "Offene Rechnungen",
     ],
-    'fahrzeug' => [
-        'file' => 'fahrzeug.php',
-        'name' => 'Fahrzeuge',
+    "fahrzeug" => [
+        "file" => "fahrzeug.php",
+        "name" => "Fahrzeuge",
     ],
-    'listmaker' => [
-        'file' => 'listmaker.php',
-        'name' => 'Listen',
+    "listmaker" => [
+        "file" => "listmaker.php",
+        "name" => "Listen",
     ],
-    'pdf' => [
-        'file' => 'pdf',
-        'name' => 'pdf',
+    "pdf" => [
+        "file" => "pdf",
+        "name" => "pdf",
     ],
-    'einstellungen' => [
-        'file' => 'einstellungen.php',
-        'name' => 'Einstellungen',
+    "einstellungen" => [
+        "file" => "einstellungen.php",
+        "name" => "Einstellungen",
     ],
-    'changelog' => [
-        'file' => 'changelog.php',
-        'name' => 'Changelog',
+    "changelog" => [
+        "file" => "changelog.php",
+        "name" => "Changelog",
     ],
-    'mitarbeiter' => [
-        'file' => 'mitarbeiter.php',
-        'name' => 'Mitarbeiter',
+    "mitarbeiter" => [
+        "file" => "mitarbeiter.php",
+        "name" => "Mitarbeiter",
     ],
-    'functionalities' => [
-        'file' => 'funktionen.php',
-        'name' => 'Funktionsübersicht',
+    "functionalities" => [
+        "file" => "funktionen.php",
+        "name" => "Funktionsübersicht",
     ],
-    'payments' => [
-        'file' => 'zahlungen.php',
-        'name' => 'Zahlungen und Rechnungen',
+    "payments" => [
+        "file" => "zahlungen.php",
+        "name" => "Zahlungen und Rechnungen",
     ],
-    'wiki' => [
-        'file' => 'wiki.php',
-        'name' => 'Firmenwiki',
+    "wiki" => [
+        "file" => "wiki.php",
+        "name" => "Firmenwiki",
     ],
-    'sticker-overview' => [
-        'file' => 'stickerOverview.php',
-        'name' => 'Motivübersicht',
+    "sticker-overview" => [
+        "file" => "stickerOverview.php",
+        "name" => "Motivübersicht",
     ],
-    'zeiterfassung' => [
-        'file' => 'timeTracking.php',
-        'name' => 'Zeiterfassung',
+    "zeiterfassung" => [
+        "file" => "timeTracking.php",
+        "name" => "Zeiterfassung",
     ],
-    'sticker' => [
-        'file' => 'sticker.php',
-        'name' => 'Sticker',
+    "sticker" => [
+        "file" => "sticker.php",
+        "name" => "Sticker",
     ],
-    'tags-overview' => [
-        'file' => 'tagsOverview.php',
-        'name' => 'Tag-Übersicht',
+    "tags-overview" => [
+        "file" => "tagsOverview.php",
+        "name" => "Tag-Übersicht",
     ],
-    'sticker-images' => [
-        'file' => 'stickerImages.php',
-        'name' => 'Bildübersicht der Motive',
+    "sticker-images" => [
+        "file" => "stickerImages.php",
+        "name" => "Bildübersicht der Motive",
     ],
-    'testpage' => [
-        'file' => 'test.php',
-        'name' => 'Test',
+    "testpage" => [
+        "file" => "test.php",
+        "name" => "Test",
     ],
-    'customer-overview' => [
-        'file' => 'customerOverview.php',
-        'name' => 'Kundenübersicht',
+    "customer-overview" => [
+        "file" => "customerOverview.php",
+        "name" => "Kundenübersicht",
     ],
-    'order-overview' => [
-        'file' => 'orderOverview.php',
-        'name' => 'Auftragsübersicht',
+    "order-overview" => [
+        "file" => "orderOverview.php",
+        "name" => "Auftragsübersicht",
     ],
+    "notifications" => [
+        "file" => "notifications.php",
+        "name" => "Benachrichtigungen",
+    ]
 ];
